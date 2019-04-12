6c84b407d16b6b146881c94489162b0908b1d1a2
annaawang <anna.a.wang@gmail.com>
2019-04-12 18:35:47 -0500

    Updates

---
+++ precache-manifest.8782ad92bc56aad3e9d6b781dbd15946.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c202f5d977b2bd95acc7cd3ff5f32db",
+    "url": "/weatherapp/static/media/clear-day.6c202f5d.svg"
+  },
+  {
+    "revision": "135e690a6ee3097f6605",
+    "url": "/weatherapp/static/css/main.23e7e7f5.chunk.css"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "cf17c4d59262e482316e",
+    "url": "/weatherapp/static/js/2.5b363b31.chunk.js"
+  },
+  {
+    "revision": "9b39372416fcb9bcf4d1723ce6aaadf3",
+    "url": "/weatherapp/static/media/rain.9b393724.svg"
+  },
+  {
+    "revision": "7b077fa11309bcbac005cf92b6dbfd5b",
+    "url": "/weatherapp/static/media/partly-cloudy-day.7b077fa1.svg"
+  },
+  {
+    "revision": "135e690a6ee3097f6605",
+    "url": "/weatherapp/static/js/main.7001e50b.chunk.js"
+  },
+  {
+    "revision": "e32e446d885dd69c7177063a04f78286",
+    "url": "/weatherapp/static/media/cloud.e32e446d.svg"
+  },
+  {
+    "revision": "aebb91ed8b65700af251632ca650c982",
+    "url": "/weatherapp/static/media/location-on-material.aebb91ed.svg"
+  },
+  {
+    "revision": "6dd7ae061b3ec807446d9fbcb50ba87d",
+    "url": "/weatherapp/static/media/dallas@3x.6dd7ae06.png"
+  },
+  {
+    "revision": "ab3021f6ecd01823af6929a0256119fd",
+    "url": "/weatherapp/static/media/thunderstorm.ab3021f6.svg"
+  },
+  {
+    "revision": "b6a7b5c8476872d8666d822113b754be",
+    "url": "/weatherapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.473616381b88aaf76de286be6ffac5a3.js"
+  "/weatherapp/precache-manifest.8782ad92bc56aad3e9d6b781dbd15946.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weather-app/index.html", {
+workbox.routing.registerNavigationRoute("/weatherapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

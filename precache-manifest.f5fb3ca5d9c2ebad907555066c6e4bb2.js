01fc213e7630ceddf008f8a180eb4262d02df304
annaawang <anna.a.wang@gmail.com>
2019-04-12 18:28:53 -0500

    Updates

---
+++ precache-manifest.f5fb3ca5d9c2ebad907555066c6e4bb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c202f5d977b2bd95acc7cd3ff5f32db",
+    "url": "/projects/weather-app/static/media/clear-day.6c202f5d.svg"
+  },
+  {
+    "revision": "135e690a6ee3097f6605",
+    "url": "/projects/weather-app/static/css/main.23e7e7f5.chunk.css"
+  },
+  {
+    "revision": "4e85090c03fb4a5708b4",
+    "url": "/projects/weather-app/static/js/runtime~main.f5f18cd0.js"
+  },
+  {
+    "revision": "cf17c4d59262e482316e",
+    "url": "/projects/weather-app/static/js/2.5b363b31.chunk.js"
+  },
+  {
+    "revision": "9b39372416fcb9bcf4d1723ce6aaadf3",
+    "url": "/projects/weather-app/static/media/rain.9b393724.svg"
+  },
+  {
+    "revision": "7b077fa11309bcbac005cf92b6dbfd5b",
+    "url": "/projects/weather-app/static/media/partly-cloudy-day.7b077fa1.svg"
+  },
+  {
+    "revision": "135e690a6ee3097f6605",
+    "url": "/projects/weather-app/static/js/main.7001e50b.chunk.js"
+  },
+  {
+    "revision": "e32e446d885dd69c7177063a04f78286",
+    "url": "/projects/weather-app/static/media/cloud.e32e446d.svg"
+  },
+  {
+    "revision": "aebb91ed8b65700af251632ca650c982",
+    "url": "/projects/weather-app/static/media/location-on-material.aebb91ed.svg"
+  },
+  {
+    "revision": "6dd7ae061b3ec807446d9fbcb50ba87d",
+    "url": "/projects/weather-app/static/media/dallas@3x.6dd7ae06.png"
+  },
+  {
+    "revision": "ab3021f6ecd01823af6929a0256119fd",
+    "url": "/projects/weather-app/static/media/thunderstorm.ab3021f6.svg"
+  },
+  {
+    "revision": "92f42d66b785a6821b32a502032f43d2",
+    "url": "/projects/weather-app/index.html"
+  }
+];
